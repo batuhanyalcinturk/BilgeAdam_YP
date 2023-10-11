35b1f8baddf273f0a0784c711395147af55becaa
Batuhan Yalçıntürk <batuhanyalcinturk@gmail.com>
2023-10-11 10:37:48 +0300

    Abstract Class Challenge

--- Odev2/src/main/java/org/example/vehicle/car/Car.java
+++ Odev2/src/main/java/org/example/vehicle/car/Car.java
@@ -2,7 +2,7 @@ package org.example.vehicle.car;
 
 import org.example.vehicle.Vehicle;
 
-abstract public class Car extends Vehicle {
+abstract class Car extends Vehicle {
 
     private final Integer modelYili;
 
